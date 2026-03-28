9569a1f8fc5443471bce7c98a2fc7ec449483126
laurenceoliver <laurenceoliverhernandez@gmail.com>
2026-03-28 09:50:32 +0800

    add new project

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 INSERT INTO project (project_name) VALUES ('Project A');
 INSERT INTO project (project_name) VALUES ('Project B');
+INSERT INTO project (project_name) VALUES ('Project C');
 
 INSERT INTO developer (developer_name, email) VALUES ('Alice Johnson', 'alice.johnson@example.com');
 INSERT INTO developer (developer_name, email) VALUES ('Ben Martinez', 'ben.martinez@example.com');
